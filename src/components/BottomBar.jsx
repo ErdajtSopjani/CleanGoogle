f9f0c9508536e4c16ed62ace78b38f3cc9eee9e7
ErdajtSopjani <erdajt.sopjani@bgt.school>
2024-06-02 23:12:21 +0200

    Added redirect links to bottombar

--- src/components/BottomBar.jsx
+++ src/components/BottomBar.jsx
@@ -5,16 +5,16 @@ const BottomBar = () => {
     return (
         <div className="absolute bottom-0 bg-neutral-900 w-screen h-11 flex flex-row justify-between text-center items-center">
             <div className="mx-auto flex flex-row justify-center text-center items-center">
-                <a className="p-4 bg-transparent text-white text-sm md:text-base hover:underline">About</a>
-                <a className="p-4 bg-transparent text-white text-sm md:text-base hover:underline">Advertising</a>
-                <a className="p-4 bg-transparent text-white text-sm md:text-base hover:underline">Business</a>
-                <a className="p-4 bg-transparent text-white text-sm md:text-base hover:underline">How Search Works</a>
+                <a className="p-4 bg-transparent text-white text-xs md:text-base hover:underline" href="https://github.com/ErdajtSopjani/CleanGoogle">About</a>
+                <a className="p-4 bg-transparent text-white text-xs md:text-base hover:underline" href="https://ads.google.com/intl/en_zz/home/">Advertising</a>
+                <a className="p-4 bg-transparent text-white text-xs md:text-base hover:underline" href="https://www.google.com/services/">Business</a>
+                <a className="p-4 bg-transparent text-white text-xs md:text-base hover:underline" href="https://github.com/ErdajtSopjani/CleanGoogle/blob/main/README.md">How Search Works</a>
             </div>
 
             <div className="mx-auto flex flex-row justify-center text-center items-center">
-                <a className="p-4 bg-transparent text-white text-sm md:text-base hover:underline">Privacy</a>
-                <a className="p-4 bg-transparent text-white text-sm md:text-base hover:underline">Terms</a>
-                <a className="p-4 bg-transparent text-white text-sm md:text-base hover:underline">Settings</a>
+                <a className="p-4 bg-transparent text-white text-xs md:text-base hover:underline" href="https://policies.google.com/privacy">Privacy</a>
+                <a className="p-4 bg-transparent text-white text-xs md:text-base hover:underline" href="https://policies.google.com/terms">Terms</a>
+                <a className="p-4 bg-transparent text-white text-xs md:text-base hover:underline" href="https://github.com/ErdajtSopjani/CleanGoogle">Settings</a>
             </div>
         </div>
 
