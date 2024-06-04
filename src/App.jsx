41c063509eba3f01c75b2358ac3c0c5bd649f4b1
ErdajtSopjani <erdajt.sopjani@bgt.school>
2024-06-04 11:52:22 +0200

    Fixed minor bug on OpenNewWindow toggle - Fixed small typo and small inconsistent value bug due to it being true by default - Slightly changed text for better context

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,12 @@ import BottomBar from "./components/Bars/BottomBar";
 
 function App() {
     const [imageSearch, setImageSearch] = useState(false);
-    const [openNewWindow, setOpenNewWindow] = useState(true);
+    const [openNewWindow, setOpenNewWindow] = useState(false);
 
     return (
         <div className={`flex flex-col items-center justify-center ${imageSearch ? "bg-slate-800/15" : "bg-slate-950/15"} w-screen h-screen overflow-hidden`}>
             <TopBar imageSearch={imageSearch} setImageSearch={setImageSearch} openNewWindow={openNewWindow} setOpenNewWindow={setOpenNewWindow} />
-            <Search imageSearch={imageSearch} openNewWindo={openNewWindow} />
+            <Search imageSearch={imageSearch} openNewWindow={openNewWindow} />
             <BottomBar />
         </div>
     );
